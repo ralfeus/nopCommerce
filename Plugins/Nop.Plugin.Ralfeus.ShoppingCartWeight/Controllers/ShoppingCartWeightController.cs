3309634d9fe9a6ae0a82affb260819429c824d5a
Mykhaylo Khodorev <ralfeus@gmail.com>
2017-06-25 16:58:19 +0200

    Shopping cart weight widget

---
+++ Plugins/Nop.Plugin.Ralfeus.ShoppingCartWeight/Controllers/ShoppingCartWeightController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Nop.Core;
+using Nop.Core.Plugins;
+using Nop.Plugin.Ralfeus.ShoppingCartWeight.Models;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Ralfeus.ShoppingCartWeight.Controllers
+{
+    public class ShoppingCartWeightController : BasePluginController
+    {
+        private readonly IWorkContext _workContext;
+
+        public ShoppingCartWeightController(IWorkContext workContext)
+        {
+            _workContext = workContext;
+        }
+
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            if (!this._workContext.CurrentCustomer.HasShoppingCartItems)
+            {
+                return null;
+            }
+            var model = new ShoppingCartWeightModel
+            {
+                Weight = this._workContext.CurrentCustomer.ShoppingCartItems.Sum(item => item.Product.Weight * item.Quantity)
+            };
+            return PartialView(model);
+        }
+    }
+}
---
+++ Plugins/Nop.Plugin.Ralfeus.ShoppingCartWeight/Models/ShoppingCartWeightModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Nop.Plugin.Ralfeus.ShoppingCartWeight.Models
+{
+    public class ShoppingCartWeightModel
+    {
+        public decimal Weight { get; set; }
+    }
+}
